798257901fbb3775a77691cd493e120d832af874
dosecare <s2210206@pharm.alayen.edu.iq>
2026-09-06 02:32:48 +0300

    V2: remove non-liquid powder products from central loader

--- dosecare-v2/js/database-loader.js
+++ dosecare-v2/js/database-loader.js
@@ -8,7 +8,7 @@
     'amoxicillin.js','amoxicillin-clavulanate.js','azithromycin.js','cephalexin.js','cefuroxime.js','cefixime.js','cefpodoxime.js','cefdinir.js','cefprozil.js','clarithromycin.js','clindamycin.js','cefaclor.js','erythromycin.js','metronidazole.js','ors.js',
     'cetirizine.js','loratadine.js','desloratadine.js','chlorpheniramine.js','fexofenadine.js','diphenhydramine.js',
     'ondansetron.js','prednisolone.js','salbutamol.js',
-    'lactulose.js','macrogol.js','omeprazole.js','magnesium-hydroxide.js','famotidine.js','sulfamethoxazole-trimethoprim.js','zinc-sulfate.js','domperidone.js','simethicone.js','probiotics.js','hyoscine-butylbromide.js'
+    'lactulose.js','omeprazole.js','magnesium-hydroxide.js','famotidine.js','sulfamethoxazole-trimethoprim.js','zinc-sulfate.js','domperidone.js','simethicone.js','hyoscine-butylbromide.js'
   ];
   function loadScript(src) {
     return new Promise((resolve, reject) => {
