612899c7c8fbf9af32885b419f1a77fa1ca9260d
dosecare <s2210206@pharm.alayen.edu.iq>
2026-09-18 17:06:37 +0300

    Trigger V2 Pages deployment with refreshed calculator assets

--- dosecare-v2/js/database-loader.js
+++ dosecare-v2/js/database-loader.js
@@ -4,7 +4,7 @@
  */
 (function (global) {
   'use strict';
-  const ASSET_VERSION = '2026-09-18-fix5';
+  const ASSET_VERSION = '2026-09-18-fix6';
   const medicineFiles = [
     'paracetamol.js','ibuprofen.js','mefenamic-acid.js','ambroxol.js','carbocisteine.js','bromhexine.js','guaifenesin.js','dextromethorphan.js',
     'amoxicillin.js','amoxicillin-clavulanate.js','azithromycin.js','cephalexin.js','cefuroxime.js','cefixime.js','cefpodoxime.js','cefdinir.js','cefprozil.js','clarithromycin.js','clindamycin.js','cefaclor.js','erythromycin.js','metronidazole.js',
