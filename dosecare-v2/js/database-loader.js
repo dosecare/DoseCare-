55cfcbef5ac726697cb6704945b6ee43bc383820
dosecare <s2210206@pharm.alayen.edu.iq>
2026-09-19 12:22:51 +0300

    Load nystatin in DoseCare V2 database

--- dosecare-v2/js/database-loader.js
+++ dosecare-v2/js/database-loader.js
@@ -7,7 +7,7 @@
   const ASSET_VERSION = '2026-09-19-oseltamivir1';
   const medicineFiles = [
     'paracetamol.js','ibuprofen.js','mefenamic-acid.js','ambroxol.js','carbocisteine.js','bromhexine.js','guaifenesin.js','dextromethorphan.js',
-    'amoxicillin.js','amoxicillin-clavulanate.js','azithromycin.js','cephalexin.js','cefuroxime.js','cefixime.js','cefpodoxime.js','cefdinir.js','cefprozil.js','clarithromycin.js','clindamycin.js','cefaclor.js','erythromycin.js','metronidazole.js','oseltamivir.js',
+    'amoxicillin.js','amoxicillin-clavulanate.js','azithromycin.js','cephalexin.js','cefuroxime.js','cefixime.js','cefpodoxime.js','cefdinir.js','cefprozil.js','clarithromycin.js','clindamycin.js','cefaclor.js','erythromycin.js','metronidazole.js','oseltamivir.js','nystatin.js',
     'cetirizine.js','loratadine.js','desloratadine.js','chlorpheniramine.js','fexofenadine.js','diphenhydramine.js',
     'ondansetron.js','prednisolone.js','salbutamol.js',
     'lactulose.js','omeprazole.js','magnesium-hydroxide.js','famotidine.js','sulfamethoxazole-trimethoprim.js','zinc-sulfate.js','domperidone.js','simethicone.js','hyoscine-butylbromide.js'
